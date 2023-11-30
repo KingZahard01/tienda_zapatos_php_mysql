1624cf8513de5142df3d173fa8ccbeadf60dbf19
Juan Garcia <juanjose10312@gmail.com>
2023-11-30 14:15:25 -0700

    pequeños arregloes al header y a db

--- db.php
+++ db.php
@@ -5,7 +5,7 @@ $username = "root";
 $password = "KG5rQWK^^u!pHbz3";
 $dbname = "nombre_tienda";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
--- includes/header.php
+++ includes/header.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <title>Tienda de Zapatos</title>
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
+
 <body>
     <!-- <header data-bs-theme="dark">
         <div class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
@@ -28,5 +30,21 @@
             <form action="search.php" method="GET" class="col-12 col-lg-auto mb-3 mb-lg-0">
                 <input class="form-control mr-sm-2" type="text" name="query" placeholder="Buscar zapatos">
             </form>
+            
+            <!-- Example single secondary button -->
+            <div class="btn-group">
+                <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                Buscar por tipo
+                </button>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Casual</a></li>
+                    <li><a class="dropdown-item" href="#">Trabajo</a></li>
+                    <li><a class="dropdown-item" href="#">Desporte</a></li>
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+                    <li><a class="dropdown-item" href="#">Todos</a></li>
+                </ul>
+            </div>
         </div>
     </header>
